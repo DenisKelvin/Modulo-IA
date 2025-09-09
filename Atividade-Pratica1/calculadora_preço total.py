46d04b394d88d3cc460a17d863984f41aa0135fa
DenisKelvin <kelvinlima.lima06@gmail.com>
2025-09-09 11:12:01 -0300

    Update calculadora_preço total.py

--- Atividade-Pratica1/calculadora_preço total.py
+++ Atividade-Pratica1/calculadora_preço total.py
@@ -2,6 +2,6 @@ nome_produto = "Cadeira infantil"
 preco_unitario = 12.40
 quantidade = 3
 
-preco_total = preco_unitario < quantidade
+preco_total = preco_unitario * quantidade
 
 print(preco_total)
