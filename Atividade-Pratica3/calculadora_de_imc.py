45a2c52dc14413e84031efd663d4b571aeacd000
DenisKelvin <kelvinlima.lima06@gmail.com>
2025-09-10 12:41:03 -0300

    Uso de if, elif e else

---
+++ Atividade-Pratica3/calculadora_de_imc.py
@@ -0,0 +1,20 @@
+peso = float(input("Digite seu peso. "))
+altura = float(input("Digite sua altura. "))
+
+imc = peso / (altura * altura)
+print(f"Seu imc é: {imc:.2f}")
+
+if imc <= 18.5:
+    print("Voce esta abaixo do peso. ")
+
+elif imc <= 25.0:
+    print("peso normal. ")
+
+elif imc <= 30.0:
+    print("Você esta acima do peso. ")
+
+elif imc <= 0:
+    print("Peso invalido. ")
+
+else:
+    print("Você esta obeso. ")
